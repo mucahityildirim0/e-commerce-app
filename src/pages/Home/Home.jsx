2ec39e27d6a43e933a5d3194c75451e6752a562d
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-12-04 16:29:31 +0300

    add product list and single product page design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Categories from "../../components/Categories/Categories";
+import Contact from "../../components/Contact/Contact";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Slider from "../../components/Slider/Slider";
 import "./home.scss";
@@ -11,6 +12,7 @@ const Home = () => {
       <FeaturedProducts type="featured" />
       <Categories />
       <FeaturedProducts type="trending" />
+      <Contact />
     </div>
   );
 };
